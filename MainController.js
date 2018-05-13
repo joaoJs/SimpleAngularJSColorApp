c263ac41575b6a96f248d724a87985da9941ebe8
Joao Campos <j.campos4g@gmail.com>
2018-05-13 14:26:23 -0400

    added some style to form and table

--- MainController.js
+++ MainController.js
@@ -4,41 +4,17 @@
   var app = angular.module("colorApp");
 
 
-  app.controller("MainController", 
-                ["$scope", 
+  app.controller("MainController",
+                ["$scope",
                  "$interval",
                  "$location",
-                 function($scope, 
-                          $interval, 
+                 function($scope,
+                          $interval,
                           $location) {
 
     $scope.rgb = "";
-    $scope.countdown = 15;
-    var promise;
 
-    var decrementCountdown = function() {
-      $scope.countdown -= 1;
-      if ($scope.countdown < 1) {
-        $scope.search($scope.rgb);
-      }
-    };
-
-    var startCountdown = function() {
-      promise = $interval(decrementCountdown, 1000, $scope.countdown);
-    };
-    startCountdown();
-
-    var stopCountdown = function() {
-      if (promise) {
-        $interval.cancel(promise);
-        stop = undefined;
-      }
-    }
-    
    $scope.search = function(rgb) {
-     if (promise) {
-       $interval.cancel(promise);
-     }
      $location.path("/color/" + rgb);
    }
 
